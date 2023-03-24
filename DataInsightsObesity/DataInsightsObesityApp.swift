24cc1c6781b66d283925b875a73fdc7da71079b6
Harini <s.harini627@gmail.com>
2023-03-24 18:27:24 -0400

    Initial Commit

---
+++ DataInsightsObesity/DataInsightsObesityApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataInsightsObesityApp.swift
+//  DataInsightsObesity
+//
+//  Created by Harini Senthilkumar on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct DataInsightsObesityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
