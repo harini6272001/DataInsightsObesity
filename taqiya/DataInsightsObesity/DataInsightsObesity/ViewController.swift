de455f1c9467d23c9ab04f24afe5267abdbb35f9
Taqiya Ehsan <68214182+taqiyaehsan@users.noreply.github.com>
2023-04-05 17:25:33 -0400

    segue
    - fixed segue from landing page to dashboard for correct credentials
    - error message for incorrect credentials

--- taqiya/DataInsightsObesity/DataInsightsObesity/ViewController.swift
+++ taqiya/DataInsightsObesity/DataInsightsObesity/ViewController.swift
@@ -9,27 +9,34 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
-    
-    @IBOutlet weak var nniLogo: UIImageView!
-    @IBOutlet weak var password_outlet: UITextField!
-    @IBOutlet weak var login_button: UIButton!
-    @IBOutlet weak var username_outlet: UITextField!
+    @IBOutlet weak var nni_logo: UIImageView!
+    @IBOutlet weak var username_in: UITextField!
+    @IBOutlet weak var password_in: UITextField!
+    @IBOutlet weak var login: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
     
-    @IBAction func login_button(_ sender: Any) {
-        guard let username = username_outlet.text else {return}
-        guard let password = password_outlet.text else {return}
+    
+    @IBAction func login(_ sender: Any) {
+        guard let password = password_in.text else {return}
+        guard let username = username_in.text else {return}
         
-        print("\(authenticateUser(username: username, password: password))")
-        
-        performSegue(withIdentifier: "goToDashboard", sender: self)
+        authenticateUser(username: username, password: password)
     }
     
-        func csv(data: String) -> [[String]] {
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToDashboard" {
+            guard let vc = segue.destination as? DashboardViewController else {return}
+            self.present(vc, animated: true)
+        }
+    }
+    
+    func csv(data: String) -> [[String]] {
             var result: [[String]] = []
             let rows = data.components(separatedBy: "\n")
             for row in rows {
@@ -38,7 +45,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
             }
             return result
         }
-
+    
     func cleanRows(file:String)->String{
         var cleanFile = file
         cleanFile = cleanFile.replacingOccurrences(of: "\r", with: "\n")
@@ -49,7 +56,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     func readDataFromCSV(fileName: String)-> String! {
         let fileURL = Bundle.main.url(forResource: "dummy_auth", withExtension: "csv")
-        
+
             do {
                 var contents = try String(contentsOf: fileURL!, encoding: .utf8)
                 contents = cleanRows(file: contents)
@@ -58,30 +65,28 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 return "File Read Error for file \(fileName)"
             }
         }
-    func authenticateUser(username: String, password: String) -> String {
-        
+    
+    func authenticateUser(username: String, password: String) -> Void {
+
         var data = readDataFromCSV(fileName: "dummy_auth.csv")
         data = cleanRows(file: data!)
         let csvRows = csv(data: data!)
         let database = csvRows[1...]
-        
+
         for info in database {
             if info[0] == username {
                 if info[1] == password {
-                    return "Welcome!"
+                    performSegue(withIdentifier: "goToDashboard", sender: self)
+                    
                 } else {
-                    return "Error"
+                    print("Error")
+                    return
                 }
             }
         }
-        return "Error"
+        print("Error")
+        return
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "goToDashboard" {
-            guard let vc = segue.destination as? DashboardViewController else {return}
-            self.present(vc, animated: true)
-        }
-    }
 }
 
